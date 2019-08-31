b46ae8428ff9f8a003f8dc624232ab1459278edd
DoisLucas <pedrolucas499@gmail.com>
2019-08-31 00:27:15 -0300

    New interface

--- lib/home/home.page.dart
+++ lib/home/home.page.dart
@@ -9,45 +9,80 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   final myController = TextEditingController();
   final bloc = BlocProvider.getBloc<HomeBloc>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey,
       body: Container(
           width: MediaQuery.of(context).size.width,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Stack(
             children: <Widget>[
-              TextField(
-                controller: myController,
-              ),
-              RaisedButton(
-                onPressed: () {
-                  bloc.searchValue.add(myController.text);
-                },
-                color: Colors.pink,
-              ),
-              StreamBuilder<Country>(
-                stream: bloc.countryChoose,
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                  Country country = snapshot.data;
-                  return Column(
+              SafeArea(
+                child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text(country.nome),
-                      Text(country.capital),
-                      Image.network(country.bandeiraUrl),
+                      Text(
+                        "Bem vindo",
+                        style: TextStyle(
+                          fontSize: 35,
+                          fontFamily: 'SF-Pro-Bold',
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text("Ao seu paískipédia!",
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontFamily: 'SF-Pro-Bold',
+                            color: Colors.white,
+                          )),
                     ],
-                  );
-                },
+                  ),
+                ),
               ),
+
+              Positioned(
+                top: 500,
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 1.6,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                ),
+              )
+
+//              TextField(
+//                controller: myController,
+//              ),
+//              RaisedButton(
+//                onPressed: () {
+//                  bloc.searchValue.add(myController.text);
+//                },
+//                color: Colors.pink,
+//              ),
+//              StreamBuilder<Country>(
+//                stream: bloc.countryChoose,
+//                builder: (context, snapshot) {
+//                  if (!snapshot.hasData) {
+//                    return Center(
+//                      child: CircularProgressIndicator(),
+//                    );
+//                  }
+//                  Country country = snapshot.data;
+//                  return Column(
+//                    children: <Widget>[
+//                      Text(country.nome),
+//                      Text(country.capital),
+//                      Image.network(country.bandeiraUrl),
+//                    ],
+//                  );
+//                },
+//              ),
             ],
           )),
     );
